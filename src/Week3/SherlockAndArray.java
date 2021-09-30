07dc6d9382feed7e71bfbdbffeaba5a69d5adf4f
SONTUNGARRETT <80802090+McGARRETT77@users.noreply.github.com>
2021-09-30 22:36:59 +0700

    Update

--- src/Week3/SherlockAndArray.java
+++ src/Week3/SherlockAndArray.java
@@ -18,8 +18,8 @@ public class SherlockAndArray {
 
     public static void main(String[] args) {
         List<Integer> list1 = List.of(1, 1, 4, 1, 1);
-        List<Integer> list2 = List.of(2, 1, 0, 0);
-        List<Integer> list3 = List.of(1, 2, 3, 3);
+        List<Integer> list2 = List.of(2, 0, 0, 0);
+        List<Integer> list3 = List.of(0, 0, 2, 0);
 
         System.out.println(list1 + balancedSums(list1));
         System.out.println(list2 + balancedSums(list2));
