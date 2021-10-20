21fca95ab6ae574a36f5cbb062d0f0c8b0549bc5
SONTUNGARRETT <80802090+McGARRETT77@users.noreply.github.com>
2021-10-20 22:36:43 +0700

    Update

--- src/Week6/InsertionSort2.java
+++ src/Week6/InsertionSort2.java
@@ -1,39 +1,38 @@
 package Week6;
 
 public class InsertionSort2 {
-    static void insertionSort2(int n, int[] arr) {
-        for (int i = 2; i <= n; i++) {
-            insertionSort1(i, arr);
-        }
-    }
-
-    static void insertionSort1(int n, int[] arr) {
-        int last = arr[n - 1];
-        int i = n - 2;
-        for (; (i >= 0) && (arr[i] > last); i--) {
-            arr[i + 1] = arr[i];
-           // printArray(arr);
-        }
-        arr[i + 1] = last;
-        printArray(arr);
-    }
-
-//    public static void insertionSort2(int n, int[] arr) {
-//        for (int i = 0; i < n - 1; i++) {
-//            int j = i + 1;
-//            while (j > 0) {
-//                if(arr[j] < arr[j - 1]){
-//                    int temp = arr[j];
-//                    arr[j] = arr[j - 1];
-//                    arr[j - 1] = temp;
-//                    j--;
-//                } else {
-//                    break;
-//                }
-//            }
-//            printArray(arr);
+//    static void insertionSort2(int n, int[] arr) {
+//        for (int i = 2; i <= n; i++) {
+//            insertionSort1(i, arr);
 //        }
 //    }
+//
+//    static void insertionSort1(int n, int[] arr) {
+//        int last = arr[n - 1];
+//        int i = n - 2;
+//        for (; (i >= 0) && (arr[i] > last); i--) {
+//            arr[i + 1] = arr[i];
+//           // printArray(arr);
+//        }
+//        arr[i + 1] = last;
+//        printArray(arr);
+//    }
+
+    public static void insertionSort2(int n, int[] arr) {
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                if(arr[j] < arr[j - 1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                }
+                else {
+                    break;
+                }
+            }
+            printArray(arr);
+        }
+    }
 
     static void printArray(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
